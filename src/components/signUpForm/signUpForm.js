216bb392ba1cab3e9019046baafc743ad0055f3b
Aadil Khatri <aadil.khatri343@gmail.com>
2023-12-06 20:24:50 +0530

    Added style property to signup form

--- src/components/signUpForm/signUpForm.js
+++ src/components/signUpForm/signUpForm.js
@@ -5,15 +5,21 @@ function SignUpForm() {
   return (
     <div className="form-section">
       <form className="form">
+        <p>
+          Sign up for <br />
+          exclusive access.
+        </p>
         <input
           className="email-field"
           name="email"
           value={`Your email address`}
           type="email"
         ></input>
+
         <button className="submit-btn" type="submit">
           Get Started
         </button>
+        <h5>You’ll receive an email with an invite link to join.</h5>
       </form>
     </div>
   );
