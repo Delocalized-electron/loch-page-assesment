e3c91389e7b7c3fc19de705a919bdff72897a1a2
Aadil Khatri <aadil.khatri343@gmail.com>
2023-12-10 21:30:57 +0530

    trying carousal

--- src/components/sideInfo/sideInfo.js
+++ src/components/sideInfo/sideInfo.js
@@ -4,17 +4,42 @@ import NotificationInfo from "../notificationInfo/notificationInfo";
 import CardInfo from "../cardInfo/cardInfo";
 import PortfolioSample from "../portfolioSample/portfolioSample";
 import Testimonial from "../testimonials/testimonial";
+import Slider from "react-slick";
+import { isMobile } from "react-device-detect";
 
 function SideInfo() {
   return (
-    <div className="side-info">
-      <div className="notification-info">
-        <NotificationInfo />
-        <CardInfo />
-      </div>
-      <PortfolioSample />
-      <Testimonial />
-    </div>
+    <>
+      {isMobile && (
+        <Slider
+          dots={true} // Enable navigation dots
+          infinite={true} // Loop slides continuously
+          speed={500} // Transition speed
+        >
+          <div className="side-info">
+            <div className="notification-info">
+              <NotificationInfo />
+              <CardInfo />
+            </div>
+            <PortfolioSample />
+            <Testimonial />
+          </div>
+        </Slider>
+      )}
+      {!isMobile && (
+        // Render flexbox container directly for non-mobile
+        <div className="flex-container">
+          <div className="side-info">
+            <div className="notification-info">
+              <NotificationInfo />
+              <CardInfo />
+            </div>
+            <PortfolioSample />
+            <Testimonial />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
